19bab7d7531e24242039992835c6b0dcfa1a8c2e
oalieno <jeffrey6910@gmail.com>
2019-08-01 13:48:13 +0800

    reformat

--- minipwn.py
+++ minipwn.py
@@ -1,27 +1,36 @@
+#!/usr/bin/env python3
 import socket
 
 class remote:
     def __init__(self, host, port):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((host, port))
-        self.buffer = ""
+        self.buffer = b''
     def recvuntil(self, text):
         while text not in self.buffer:
             self.buffer += self.s.recv(1024)
         index = self.buffer.find(text) + len(text)
-        ans, self.buffer = self.buffer[:index], self.buffer[index:]
-        return ans
+        result, self.buffer = self.buffer[:index], self.buffer[index:]
+        return result
     def recvline(self):
-        return self.recvuntil('\n')
+        return self.recvuntil(b'\n')
     def recvlines(self, n):
         lines = []
         for _ in range(n):
             lines.append(self.recvline())
         return lines
+    def _convert_to_bytes(self, text):
+        if type(text) is not bytes:
+            text = str(text)
+        if type(text) is str:
+            text = text.encode()
+        return text
     def send(self, text):
+        text = self._convert_to_bytes(text)
         self.s.sendall(text)
     def sendline(self, text):
-        self.s.sendall(text + '\n')
+        text = self._convert_to_bytes(text)
+        self.send(text + b'\n')
     def sendafter(self, prefix, text):
         self.recvuntil(prefix)
         self.send(text)
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name='remote',
+    name='mini-pwntools',
     version='0.0.1',
-    description='replace of pwntools remote class for sage',
+    description='minimum pwntools with python3',
     install_requires=[],
     author='OAlienO',
     author_email='jeffrey6910@gmail.com',
-    url='https://github.com/OAlienO/remote',
+    url='https://github.com/OAlienO/mini-pwntools',
     license='MIT License',
-    py_modules=[ 'remote' ],
+    py_modules=[ 'minipwn' ],
 )
